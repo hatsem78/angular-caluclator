5fd3c0b57cacf3f1510d5697a4b08d42e13da0d9
Havas <havas@example.com>
2020-03-29 10:22:42 -0300

    Componente calculadora que es un simple ejemplo de como utilizar los componentes de anguar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+
+const routes: Routes = [
+  {
+    path: 'calculator',
+    component: CalculatorComponent,
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
